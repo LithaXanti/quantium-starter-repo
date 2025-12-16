1c174de6d67ac506d6be5e4314d2c8709c4136d2
Litha Xanti <167000135+LithaXanti@users.noreply.github.com>
2025-12-16 11:44:26 +0200

    new changes

---
+++ visualiser.py
@@ -0,0 +1,32 @@
+import pandas
+from dash import Dash, html, dcc
+
+from plotly.express import line
+
+DATA_PATH = "./formatted_data.csv"
+
+data = pandas.read_csv(DATA_PATH)
+data = data.sort_values(by="date")
+
+dash_app = Dash(__name__)
+
+line_chart = line(data, x="date", y="sales", title="Pink Morsel Sales")
+visualisation = dcc.Graph(
+    id="visualisation",
+    figure=line_chart
+)
+
+header = html.H1(
+    "Pink Morsel Visualiser",
+    id="header"
+)
+
+dash_app.layout = html.Div(
+    [
+        header,
+        visualisation
+    ]
+)
+
+if __name__ == '__main__':
+    dash_app.run_server()
